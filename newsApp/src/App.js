095cd2a16f8e7820af3742b2a933c425a76deacd
Alpagu E <alpagu872@gmail.com>
2022-08-01 21:25:12 +0300

    NewsApp Update || 9:25 PM

---
+++ newsApp/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import NewsCard from './components/NewsCard'
+
+
+function App() {
+  return (
+    <SafeAreaView>
+
+      <View>
+        <Text>Test</Text>
+        <NewsCard/>
+
+
+      </View>
+
+    </SafeAreaView>
+  )
+}
+
+export default App;
---
+++ newsApp/src/components/NewsCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text,View} from 'react-native';
+
+const NewsCard = () => {
+    return(
+        <View >
+            <Text>News Card Placeholder</Text>
+        </View>
+    )
+}
+
+export default NewsCard;
